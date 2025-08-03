4fd8f36419a3146cc46fd62bb0c8cd58218aef20
yunes errachid <yunes@Yunes.home>
2025-08-04 00:58:13 +0200

    bd avec postgres

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def charger_profil_utilisateur(email):
 
 # === Config Flask & DB ===
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///flashreviewer.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 app.secret_key = "yunes_secret_key"
